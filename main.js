318739384d02e9285d87ba7808de66a1c271d378
isanjeev23 <sk315055@gmail.com>
2022-09-07 11:47:43 +0530

    fix:duplicate rendering

--- main.js
+++ main.js
@@ -1,74 +1,63 @@
-const { createApp }  = Vue ;
+const { createApp } = Vue;
 
-let id = 0 ;
+let id = 0;
 createApp({
-    data(){
-         return{
-          newTodo : "" ,
-          todos : [] ,
-          display : false ,
-        //   completed : [],
-        
-         } 
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+      display: false,
+      //   completed : [],
+    };
+  },
+
+  methods: {
+    addTask() {
+      if (this.newTodo.length === 0) {
+        alert("Input value is null");
+
+        return;
+      }
+      this.todos.push({ id: id++, text: this.newTodo, checked: false });
+      this.newTodo = "";
+      console.log("addTask is invoked");
     },
 
-    methods : {
-        addTask(){
+    toggle(key) {
+      let idx = this.todos.findIndex((obj) => obj.id === key);
 
-            if(this.newTodo.length  === 0 ){
-                alert("Input value is null") ;
-                
-                return ;
-           }
-            this.todos.push({id : id++ , text : this.newTodo ,  checked : false }) ;
-            this.newTodo = '';
-            console.log("addTask is invoked")
-        },
+      this.todos[idx].checked = !this.todos[idx].checked;
 
-        toggle(key){
-            let idx = this.todos.findIndex((obj) => obj.id === key ) ;
+      for (let i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].checked === true) {
+          this.display = true;
+          break;
+        } else this.display = false;
+      }
 
-             this.todos[idx].checked = !this.todos[idx].checked   ;
-            
-   
+      //  if(this.todos[idx].checked){
+      //     this.completed.push(this.todos[idx]) ;
+      //  }
 
-             for(let i = 0 ; i<this.todos.length ; i++){
-                if(this.todos[i].checked === true){
-                    this.display =  true ;
-                    break ;
-                }
-                else 
-                   this.display = false
-             }
+      //  // filtering out completed task
+      //  this.todos =this.todos.filter((elem) => elem.checked == false ) ;
+      //  console.log("cmpltd is invoked") ;
 
-          
-            
-            //  if(this.todos[idx].checked){
-            //     this.completed.push(this.todos[idx]) ;
-            //  }
-             
-            //  // filtering out completed task 
-            //  this.todos =this.todos.filter((elem) => elem.checked == false ) ;
-            //  console.log("cmpltd is invoked") ;
+      //  console.log(this.todos[idx].checked);
+    },
+    removeTask(key) {
+      this.todos = this.todos.filter((elem) => elem.id != key);
+      console.log("removeTask is  invoked!");
+    },
 
-            //  console.log(this.todos[idx].checked);
-        },
-        removeTask(key){
+    editTask(key) {
+      let rename = window.prompt("Input new Todo ");
 
-            this.todos = this.todos.filter((elem) => elem.id != key ) ;
-            console.log("removeTask is  invoked!")
-        },
+      let idx = this.todos.findIndex((obj) => obj.id === key);
 
-        editTask(key){
-            let rename = window.prompt("Input new Todo ") ;
-
-            let idx = this.todos.findIndex((obj) => obj.id === key ) ;
-             
-             if(idx !== -1){
-
-                this.todos[idx].text  = rename ;
-             }
-        }
-
-    }
-}).mount("#app")
+      if (idx !== -1) {
+        this.todos[idx].text = rename;
+      }
+    },
+  },
+}).mount("#app");
